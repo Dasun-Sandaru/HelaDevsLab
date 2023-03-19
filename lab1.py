c370065f7d79477a122f6b60d032d4e0e46d67a6
Dasun-Sandaru <dasunsandaru98@gmail.com>
2023-03-19 06:42:18 +0530

    lab-1-2023_03_18

---
+++ lab1.py
@@ -0,0 +1,26 @@
+# This is comment line
+
+# display value
+print("Hello - @ 2023-03-18")
+
+
+x = 10
+y = 20
+
+z = x + y
+
+print(z)
+
+# get user inputs
+a = input("Enter value for A : ")
+b = input("Enter value for B : ")
+
+# print as string
+print(a+b) 
+
+# print as number
+a_as_int = int(a)
+b_as_int = int(b)
+
+print(a_as_int + b_as_int)
+hj
